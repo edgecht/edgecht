1e9e576b63557125b9bcaeb2394b3c19d7f07a23
Wackery <zapcontact000@gmail.com>
2021-03-31 17:28:37 -0700

    the beginings

---
+++ cht.js
@@ -0,0 +1,14 @@
+
+
+function grabRequirements(xml) {
+    if (typeof(xml) == "string") {
+        xml = new DOMParser().parseFromString(xml,"text/xml")
+    }
+    outcome = xml.getElementsByTagName("outcome")[0]
+    if (!outcome) {
+        return
+    }
+    requires = Array.from(outcome.children)
+    requires = requires.map((x) => x.innerHTML)
+    return requires
+}
