5ac44dc8966aa16458cc19d2bff43240f5b382e2
Restyled.io <commits@restyled.io>
2021-04-12 19:43:01 +0000

    Restyled by prettier

--- cht.js
+++ cht.js
@@ -2,10 +2,11 @@
 // why would you keep the correct answers on the client?
 // just why
 window.edgecht = {
-  isinit : false,
-  removecdata : function(
-      data) { return data.replace("<![CDATA[", "").replace("]]>", "") },
-  grabRequirements : function(xml) {
+  isinit: false,
+  removecdata: function (data) {
+    return data.replace("<![CDATA[", "").replace("]]>", "");
+  },
+  grabRequirements: function (xml) {
     if (typeof xml == "string") {
       xml = new DOMParser().parseFromString(xml, "text/xml");
     }
@@ -16,78 +17,90 @@ window.edgecht = {
     requires = Array.from(outcome.children).map((x) => x.innerHTML);
     return requires;
   },
-  formatXMLChoice : function(element) {
+  formatXMLChoice: function (element) {
     return {
       id: element.getAttribute("id"),
-          type: element.hasAttribute("type") ? element.getAttribute("type")
-                                             : "checkbox/radio",
-          contents: element.getElementsByTagName("content").length != 0
-              ? edgecht.removecdata(
-                    element.getElementsByTagName("content")[0].innerHTML)
-              : null // why
-    }
+      type: element.hasAttribute("type")
+        ? element.getAttribute("type")
+        : "checkbox/radio",
+      contents:
+        element.getElementsByTagName("content").length != 0
+          ? edgecht.removecdata(
+              element.getElementsByTagName("content")[0].innerHTML
+            )
+          : null, // why
+    };
   },
-  fromID : function(progress, task, id) {
+  fromID: function (progress, task, id) {
     task = this.getTaskDoc(progress, task);
     ida = id.split(":").pop();
     return task.getElementById(ida);
   },
 
-  getAPI : function() { return this.getContentWindow().API; },
-  getContentWindow : function() { return $("#stageFrame")[0].contentWindow; },
-  getInternalIFrame : function() {
+  getAPI: function () {
+    return this.getContentWindow().API;
+  },
+  getContentWindow: function () {
+    return $("#stageFrame")[0].contentWindow;
+  },
+  getInternalIFrame: function () {
     return this.getContentWindow().$("#iFramePreview")[0].contentWindow;
   },
-  getItemFromID : function(id) {
+  getItemFromID: function (id) {
     id = id.split(":").pop();
     return this.getInternalIFrame().$("[value='" + id + "']")[0];
   },
-  getFrame : function() { return this.getAPI().Frame; },
-  getStack : function(
-      progress) { return this.getFrame().StackProgress[progress]; },
-  getTask : function(
-      progress,
-      task) { return this.getStack(progress).TaskProgress[task].Text; },
-  getTaskDoc : function(progress, task) {
-    return new DOMParser().parseFromString(this.getTask(progress, task),
-                                           "text/xml");
+  getFrame: function () {
+    return this.getAPI().Frame;
   },
-  grabTaskRequirements : function(
-      progress,
-      task) { return this.grabRequirements(this.getTask(progress, task)); },
-  parseTaskRequirements : function(taskRequirements) {
+  getStack: function (progress) {
+    return this.getFrame().StackProgress[progress];
+  },
+  getTask: function (progress, task) {
+    return this.getStack(progress).TaskProgress[task].Text;
+  },
+  getTaskDoc: function (progress, task) {
+    return new DOMParser().parseFromString(
+      this.getTask(progress, task),
+      "text/xml"
+    );
+  },
+  grabTaskRequirements: function (progress, task) {
+    return this.grabRequirements(this.getTask(progress, task));
+  },
+  parseTaskRequirements: function (taskRequirements) {
     taskreq = [];
     bigtask = taskRequirements.join("|").split("|");
-    amountof = bigtask.length / 3 // amount of requirements
+    amountof = bigtask.length / 3; // amount of requirements
     for (task = 0; task < amountof; task++) {
       thistask = {
-        eleid : bigtask.shift(),
-        type : bigtask.shift(),
-        value : bigtask.shift()
+        eleid: bigtask.shift(),
+        type: bigtask.shift(),
+        value: bigtask.shift(),
       };
       taskreq.push(thistask);
-    };
-    return taskreq
+    }
+    return taskreq;
   },
-  parseTask : function(progress, task) {
+  parseTask: function (progress, task) {
     rawtask = this.getTaskDoc(progress, task);
     requireTask = this.parseTaskRequirements(this.grabRequirements(rawtask));
-    contents = Array.from(rawtask.getElementsByTagName("content"))
+    contents = Array.from(rawtask.getElementsByTagName("content"));
     /*contents.pop() // remove ending div
     contents.shift() // remove start of div*/
     task = {
-      requirements : requireTask,
-      type : rawtask.getElementsByTagName("type")[0].innerHTML,
-      question : this.removecdata(
-          contents[1].innerHTML), // this is garbage, fails sometimes, fix this
-                                  // by looking for one that doesnt start with
-                                  // <div> maybe?
-      answers : Array.from(rawtask.getElementsByTagName("choice"))
-                    .map(this.formatXMLChoice)
+      requirements: requireTask,
+      type: rawtask.getElementsByTagName("type")[0].innerHTML,
+      question: this.removecdata(contents[1].innerHTML), // this is garbage, fails sometimes, fix this
+      // by looking for one that doesnt start with
+      // <div> maybe?
+      answers: Array.from(rawtask.getElementsByTagName("choice")).map(
+        this.formatXMLChoice
+      ),
     };
-    return task
+    return task;
   },
-  init : function() {
+  init: function () {
     if (this.isinit) {
       console.warn("Uh oh already started in this session");
       return;
