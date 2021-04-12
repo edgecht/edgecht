599ac58cc10eb4cd5134ec3321301471a5e79059
Restyled.io <commits@restyled.io>
2021-04-12 15:39:21 +0000

    Restyled by clang-format

--- cht.js
+++ cht.js
@@ -1,64 +1,56 @@
 window.edgecht = {
-    isinit: false,
-    grabRequirements: function (xml) {
-    if (typeof(xml) == "string") {
-        xml = new DOMParser().parseFromString(xml,"text/xml")
+  isinit : false,
+  grabRequirements : function(xml) {
+    if (typeof (xml) == "string") {
+      xml = new DOMParser().parseFromString(xml, "text/xml")
     }
     outcome = xml.getElementsByTagName("outcome")[0];
     if (!outcome) {
-        return
+      return
     }
     requires = Array.from(outcome.children).map((x) => x.innerHTML);
     return requires
-},
-fromID: function(progress,task,id) {
-    task = this.getTaskDoc(progress,task);
+  },
+  fromID : function(progress, task, id) {
+    task = this.getTaskDoc(progress, task);
     ida = id.split(":").pop();
     return task.getElementById(ida);
-},
+  },
 
-getAPI: function () {
-    return this.getContentWindow().API;
-},
-getContentWindow: function () {
-    return $("#stageFrame")[0].contentWindow;
-},
-getInternalIFrame: function () {
+  getAPI : function() { return this.getContentWindow().API; },
+  getContentWindow : function() { return $("#stageFrame")[0].contentWindow; },
+  getInternalIFrame : function() {
     return this.getContentWindow().$("#iFramePreview")[0].contentWindow;
-},
-getItemFromID: function (id) {
+  },
+  getItemFromID : function(id) {
     id = id.split(":").pop();
-    return this.getInternalIFrame().$("[value='"+id+"']")[0]
-},
-getFrame: function () {
-    return this.getAPI().Frame;
-},
-getStack: function (progress) {
+    return this.getInternalIFrame().$("[value='" + id + "']")[0]
+  },
+  getFrame : function() { return this.getAPI().Frame; },
+  getStack : function(progress) {
     return this.getFrame().StackProgress[progress];
+  },
+  getTask : function(
+      progress,
+      task) { return this.getStack(progress).TaskProgress[task].Text; },
+  getTaskDoc : function(progress, task) {
+    return new DOMParser().parseFromString(this.getTask(progress, task),
+                                           'text/xml');
+  },
+  grabTaskRequirements : function(progress, task) {
+    return this.grabRequirements(this.getTask(progress, task));
+  },
 
-},
-getTask: function (progress,task) {
-    return this.getStack(progress).TaskProgress[task].Text;
-},
-getTaskDoc: function (progress,task) {
-    return new DOMParser().parseFromString(this.getTask(progress,task),'text/xml');
-},
-grabTaskRequirements: function (progress,task) {
-    return this.grabRequirements(this.getTask(progress,task));
-
-},
-
-init: function () {
+  init : function() {
     if (!this.isinit) {
-        return
+      return
     }
     console.log("EDGECHT");
     console.log("Because Edgenuity's Coders are garbage");
     this.isinit = true
     //@TODO Actually do stuff here
     //@Body lmao
-}
+  }
 }
 
-edgecht.init();
-
+                 edgecht.init();
