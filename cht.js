a223f6a8f44593dd0616e3a31fafece45aaba228
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-05-03 12:06:55 -0700

    Update cht.js

--- cht.js
+++ cht.js
@@ -6,6 +6,20 @@ window.edgecht = {
   removecdata: function (data) {
     return data.replace("<![CDATA[", "").replace("]]>", "");
   },
+  ordinal_suffix_of: function (i) { // from https://stackoverflow.com/questions/13627308/add-st-nd-rd-and-th-ordinal-suffix-to-a-number
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+},
   grabRequirements: function (xml) {
     if (typeof xml == "string") {
       xml = new DOMParser().parseFromString(xml, "text/xml");
