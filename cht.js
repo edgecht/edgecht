1689650339d3b854124d3bd767ccae3c30cfad90
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-05-03 11:55:54 -0700

    Update cht.js

--- cht.js
+++ cht.js
@@ -120,6 +120,9 @@ window.edgecht = {
     };
     return task;
   },
+  displayUserMessage: function (progress, task) {
+    alert(this.createMessageForCorrectAnswer(progress, task)) // temp before i get a ui lmao
+  }
   init: function () {
     if (this.isinit) {
       console.warn("Uh oh already started in this session");
